a7e7b7c79b3157989e325425d13382db44fcdd6e
liquid-dre <avdingiswayo54@gmail.com>
2025-11-20 22:04:30 +0200

    Contact page done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,6 @@
 import { ContactDetails } from "./components/ContactDetails";
 import { ContactForm } from "./components/ContactForm";
 import { ContactHero } from "./components/ContactHero";
-import { LocationSpotlight } from "./components/LocationSpotlight";
-import { OpeningHours } from "./components/OpeningHours";
 
 export default function ContactPage() {
   return (
@@ -10,10 +8,6 @@ export default function ContactPage() {
       <ContactHero />
       <ContactDetails />
       <ContactForm />
-      <div className="grid gap-6 lg:grid-cols-2">
-        <OpeningHours />
-        <LocationSpotlight />
-      </div>
     </div>
   );
 }
