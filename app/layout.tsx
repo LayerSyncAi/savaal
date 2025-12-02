e5e27139231984c675b3ea7d47e9526ac1a28a99
liquid-dre <avdingiswayo54@gmail.com>
2025-12-02 20:33:41 +0200

    fixed poppins directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,11 @@ const poppins = localFont({
   variable: "--font-poppins",
   display: "swap",
   src: [
-    { path: "../public/fonts/poppins/Poppins-Light.ttf", weight: "300", style: "normal" },
-    { path: "../public/fonts/poppins/Poppins-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../public/fonts/poppins/Poppins-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../public/fonts/poppins/Poppins-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../public/fonts/poppins/Poppins-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../public/fonts/Poppins/Poppins-Light.ttf", weight: "300", style: "normal" },
+    { path: "../public/fonts/Poppins/Poppins-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../public/fonts/Poppins/Poppins-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../public/fonts/Poppins/Poppins-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../public/fonts/Poppins/Poppins-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
